fb66c46d1d553e14612f234cc7be39cc2991db7f
Gustavo Mahecha <gmahechas@ingenieros.com>
2021-12-02 11:15:23 -0500

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
-
-import app from './app';
+import jsforce from 'jsforce';
 
 const start = async () => {
-	app.listen(3000, () => console.log(`runnig on port: ${3000}`));
-};
+	const soapOptions = {
+		clientId: '',
+		clientSecret: '',
+		loginUrl: '',
+	}
+	const soapClient = new jsforce.Connection(soapOptions);
+	const userInfo = await soapClient.login('', ''); //password + security token
+
+	// describe
+	/* 	const sobject: jsforce.SObject<any> = soapClient.sobject('Customer_Pricing_Table__c');
+		const describe = await sobject.describe(); */
+
+ 	const productsQuery = await soapClient.query("SELECT Id, Name, Description, Standard_Price__c, Minimum_Price__c FROM Product2 WHERE ProductCode=''");
+	console.log(productsQuery.records);
+
+	/*const create = await soapClient.sobject('Customer_Pricing_Table__c').create({
+		Product__c: '01t3J000002vtCAQAY',
+		Account__c: '001G0000010cgKbIAI',
+		Customer_Price__c: 25
+	});
+
+	console.log(create); */
+}
 
 start();
--- src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { salesForceRouter } from './salesforce.routes';
-
-const router = express.Router();
-
-export const routes = [
-	router.use('/salesforce', salesForceRouter),
-];
